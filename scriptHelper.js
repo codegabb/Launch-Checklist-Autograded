c3745fee9ce7bd3e4d59404f99810834171a6d3f
codegabb <gab.blenden@gmail.com>
2024-03-19 12:32:47 -0500

    GA Assignment 5- Launch Checklist

--- scriptHelper.js
+++ scriptHelper.js
@@ -14,7 +14,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                      <li>Distance from Earth: ${distance} </li>
                      <li>Number of Moons: ${moons}</li>
                  </ol>
-                 <img src="${imageUrl}">`
+                 <img src= ${imageUrl}>`
              
             
  }
